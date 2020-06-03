c65e82cea32cd23013765bbf39ea144d9efd45bf
Jon Froehlich <jonfroehlich@gmail.com>
2020-06-03 12:54:53 -0700

    added some additional help visualizations to gesturerec.vis

--- Projects/GestureRecognizer/gesturerec/vis.py
+++ Projects/GestureRecognizer/gesturerec/vis.py
@@ -2,13 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 
-def plot_confusion_matrix(cm, classes, title, normalize=False, cmap=plt.cm.Blues):
+def plot_confusion_matrix(cm, classes, title, normalize=False, cmap=plt.cm.Blues, figsize=None):
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
         # print("Normalized confusion matrix")
 
     # print(cm)
-    plt.figure(figsize=(12, 6))
+    if figsize is None:
+        figsize = (12, 6)
+
+    plt.figure(figsize=figsize)
     plt.imshow(cm, interpolation='nearest', cmap=cmap) #imshow displays data on a 2D raster
     plt.title(title)
     plt.colorbar()
@@ -27,3 +30,122 @@ def plot_confusion_matrix(cm, classes, title, normalize=False, cmap=plt.cm.Blues
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
     plt.show()
+
+def plot_bar_graph(d, title=None, ylabel=None, xlabel=None):
+    '''
+    Plots a bar graph of of the values in d (with the keys as names)
+    '''
+    
+    sorted_tuple_list = sorted(d.items(), key=lambda x: x[1])
+    n_groups = len(d)
+
+    sorted_keys = [] 
+    sorted_values = [] 
+    for k, v in sorted_tuple_list:
+        sorted_keys.append(k)
+        sorted_values.append(v)
+
+    # create plot
+    fig_height = max(n_groups * 0.5, 5)
+    plt.figure(figsize=(12, fig_height))
+    indices = np.arange(len(sorted_keys))
+
+    plt.grid(zorder=0)
+    bars = plt.barh(indices, sorted_values, alpha=0.8, color='b', zorder=3)
+    
+    plt.ylabel(xlabel)
+    plt.xlabel(ylabel)
+    plt.xlim(0, sorted_values[-1] * 1.1)
+    plt.title(title)
+    plt.yticks(indices, sorted_keys)
+    
+    for i, v in enumerate(sorted_values):
+        plt.text(v + 0.01, i, "{:0.2f}".format(v), color='black', fontweight='bold')
+    
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_signals(gesture_set, signal_var_names = ['x', 'y', 'z']):
+    '''Plots the gesture set as a grid given the signal_var_names'''
+    num_rows = len(gesture_set.map_gestures_to_trials)
+    num_cols = len(signal_var_names)
+    row_height = 3.5
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, row_height * num_rows))
+    fig.subplots_adjust(hspace=0.5)
+
+    index = 0
+    for row, gesture_name in enumerate(gesture_set.get_gesture_names_sorted()):
+        gesture_trials = gesture_set.get_trials_for_gesture(gesture_name)
+
+        for trial in gesture_trials:
+            for col, signal_var_name in enumerate(signal_var_names):
+                s = getattr(trial.accel, signal_var_name)       
+                axes[row][col].plot(s, alpha=0.7, label=f"Trial {trial.trial_num}")
+
+                axes[row][col].set_title(f"{gesture_name}: {signal_var_name}")
+                axes[row][col].legend()
+
+    fig.tight_layout(pad=2)
+
+def plot_signals_aligned(gesture_set, signal_var_names = ['x', 'y', 'z'], title_fontsize=8):
+    '''Aligns each signal using cross correlation and then plots them'''
+    num_rows = len(gesture_set.map_gestures_to_trials)
+    num_cols = len(signal_var_names)
+    row_height = 3.5
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, row_height * num_rows))
+    fig.subplots_adjust(hspace=0.5)
+
+    index = 0
+    for row, gesture_name in enumerate(gesture_set.get_gesture_names_sorted()):
+        gesture_trials = gesture_set.get_trials_for_gesture(gesture_name)
+
+        for col, signal_var_name in enumerate(signal_var_names):
+            
+            # Find the maximum length signal. We need this to pad all
+            # signals to the same length (a requirement of cross correlation)
+            signal_lengths = []
+            for trial in gesture_trials:
+                s = getattr(trial.accel, signal_var_name) 
+                signal_lengths.append(len(s))
+            max_trial_length = np.max(signal_lengths)
+            
+            # Actually pad the signals
+            signals = []
+            for trial in gesture_trials:
+                s = getattr(trial.accel, signal_var_name) 
+                
+                padding_length = max_trial_length - len(s)
+                if padding_length > 0:
+                    pad_amount_left = (math.floor(padding_length/2.0))
+                    pad_amount_right = (math.ceil(padding_length/2.0))
+                    padded_s = np.pad(s, (pad_amount_left, pad_amount_right), mode = 'mean')
+                    signals.append(padded_s)
+                else:
+                    signals.append(s)
+            
+            # Grab a signal to align everything to. We could more carefully choose
+            # this signal to be the closest signal to the average aggregate... but this
+            # should do for now
+            golden_signal = signals[0] # the signal to align everything to
+            
+            # Align all the signals and store them in aligned_signals
+            aligned_signals = [golden_signal]
+            for i in range(1, len(signals)):
+                a = golden_signal
+                b = signals[i]
+                correlate_result = np.correlate(a, b, 'full')
+                best_correlation_index = np.argmax(correlate_result)
+                shift_amount = (-len(a) + 1) + best_correlation_index
+                b_shifted_mean_fill = shift_array(b, shift_amount, np.mean(b))
+                aligned_signals.append(b_shifted_mean_fill)
+            
+            # Plot the aligned signals
+            for signal_index, trial in enumerate(gesture_trials):
+                s = aligned_signals[signal_index]
+                axes[row][col].plot(s, alpha=0.7, label=f"Trial {trial.trial_num}")
+
+                axes[row][col].set_title(f"Aligned {gesture_name}: {signal_var_name}", fontsize=title_fontsize)
+                axes[row][col].legend()           
+
+    fig.tight_layout(pad=2)
